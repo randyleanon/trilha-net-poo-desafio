0e9b304efcfabba34e0372c0437ae8eb7f668e30
randyleanon <jorgelucasdlcruz@gmail.com>
2024-11-14 16:32:16 -0300

    Add files via upload

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // TODO: Herdar da classe "Smartphone" - CHECK
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // TODO: Sobrescrever o método "InstalarAplicativo" - CHECK
+        public Nokia(string Number, string Model, string IMEI, int Storage) : base(Number, Model, IMEI, Storage)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"instalando aplicativo '{nomeApp}' no teu Nokia Phone...");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,23 +2,31 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
+        public string Number { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // CHECK
+        private string Model { get; set; }
+        private string IMEI { get; set; }
+        private int Storage { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string cNumber, string cModel, string cIMEI, int cStorage)
         {
-            Numero = numero;
+            Number = cNumber;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            // CHECK
+            Model = cModel;
+            IMEI = cIMEI;
+            Storage = cStorage;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando desde " + Number + "...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação em " + Number + "...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
